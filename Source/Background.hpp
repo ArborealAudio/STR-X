9d8589530843ec27c05ee9917c62931a99db684c
Alex Riccio <alexbriccio@gmail.com>
2022-12-11 17:11:55 -0500

    attempt to fix logo not drawing correctly (works on Windows)

--- Source/Background.hpp
+++ Source/Background.hpp
@@ -19,13 +19,15 @@ struct Background : Component
     {
         g.setColour(*channel ? Colours::black : Colour(BLUE_BG));
 
-        auto textbounds = getLocalBounds().reduced(getWidth() * 0.1f, 0);
-        g.fillRoundedRectangle(textbounds.reduced(10).toFloat(), 10.f);
+        auto textbounds = getLocalBounds().reduced(getWidth() * 0.1f + 10, 10).toFloat();
+        g.fillRoundedRectangle(textbounds, 10.f);
 
-        strix->drawWithin(g, getLocalBounds().toFloat(), RectanglePlacement::centred, 1.f);
+        // strix->setTransformToFit(textbounds, RectanglePlacement::centred);
+        // strix->draw(g, 1.f);
+        strix->drawWithin(g, getLocalBounds().toFloat(), RectanglePlacement::xLeft, 1.f);
 
         g.setColour(*channel ? Colour(GREEN) : Colours::wheat);
-        g.drawRoundedRectangle(textbounds.toFloat().reduced(10), 10.f, 3.f);
+        g.drawRoundedRectangle(textbounds, 10.f, 3.f);
     }
 
 private:
