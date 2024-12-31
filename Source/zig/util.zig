f14d9c7dfd8dddb80f75b11bbd39b8a8d2330cd1
abriccio <alexbriccio@gmail.com>
2024-12-31 09:38:49 -0500

    Add param mapping fn

--- Source/zig/util.zig
+++ Source/zig/util.zig
@@ -1,3 +1,5 @@
+const std = @import("std");
+
 pub const AudioBuffer32 = struct {
     num_channels: u32,
     num_frames: u32,
@@ -14,3 +16,8 @@ pub const DoubleVec = @Vector(2, f64);
 pub const AudioBufferVector = struct {
     data: []DoubleVec,
 };
+
+// map normalized float to new range
+pub fn map(comptime T: type, x: T, min: T, max: T) T {
+    return (x * (max - min)) + min;
+}
